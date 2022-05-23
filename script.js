16bc7e5e1b60ebf4188bb09437fb303cd967d218
Richard Bernisca <taropogi_123@yahoo.com>
2022-05-24 00:10:39 +0800

    init function

--- script.js
+++ script.js
@@ -6,25 +6,70 @@ const btnHold = document.querySelector(".btn--hold");
 const imageDice = document.querySelector(".dice");
 
 //initial values
-const totalScore = [0, 0];
-let currentScore = 0;
-let activePlayer = 0; // used 0 and 1 to reference in array
+let totalScore, currentScore, activePlayer, playing;
+
+const init = function () {
+  currentScore = 0;
+  totalScore = [0, 0];
+
+  activePlayer = 0; // used 0 and 1 to reference in array
+  playing = true;
+  document.getElementById("score--0").textContent = 0;
+  document.getElementById("score--1").textContent = 0;
+  imageDice.classList.remove("hidden");
+
+  document.querySelector(`.player--0`).classList.remove("player--winner");
+  document.querySelector(`.player--1`).classList.remove("player--winner");
+};
+
+init();
+
+const switchPlayer = function () {
+  // set current score to 0
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+
+  activePlayer = activePlayer === 0 ? 1 : 0;
+
+  document.querySelector(`.player--0`).classList.toggle("player--active");
+  document.querySelector(`.player--1`).classList.toggle("player--active");
+};
 
 btnRollDice.addEventListener("click", function () {
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  imageDice.src = `dice-${dice}.png`;
+    imageDice.src = `dice-${dice}.png`;
 
-  if (dice === 1) {
-    // set current score to 0
-    currentScore = 0;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-    // switch player
-    activePlayer = activePlayer === 0 ? 1 : 0;
-  } else {
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
+    if (dice === 1) {
+      // switch player
+      switchPlayer();
+    } else {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    }
   }
 });
+
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    totalScore[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      totalScore[activePlayer];
+
+    if (totalScore[activePlayer] >= 20) {
+      // winner
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      imageDice.classList.add("hidden");
+      playing = false;
+    }
+
+    switchPlayer();
+  }
+});
+
+btnNewGame.addEventListener("click", init);
